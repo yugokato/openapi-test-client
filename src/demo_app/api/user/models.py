a8ec89864613ab55baac2a2f645e2780be2e3f53
Yugo Kato <yugokato.o@gmail.com>
2025-05-08 10:37:48 -0700

    Switch to use FastAPI for demo app

--- src/demo_app/api/user/models.py
+++ src/demo_app/api/user/models.py
@@ -1,7 +1,6 @@
 from enum import Enum
 
-from pydantic import AnyUrl, BaseModel, EmailStr, Field
-from quart_schema.pydantic import File
+from pydantic import AnyUrl, BaseModel, EmailStr, Field, PositiveInt
 
 
 class UserRole(Enum):
@@ -17,7 +16,7 @@ class UserTheme(Enum):
 
 
 class UserQuery(BaseModel):
-    id: int | None = None
+    id: PositiveInt | None = None
     email: EmailStr | None = None
     role: UserRole | None = None
 
@@ -26,13 +25,13 @@ class SocialLinks(BaseModel):
     facebook: AnyUrl | None = None
     instagram: AnyUrl | None = None
     linkedin: AnyUrl | None = None
-    github: AnyUrl | None = None
+    github: AnyUrl = None
 
 
 class Preferences(BaseModel):
     theme: UserTheme | None = UserTheme.LIGHT_MODE
     language: str | None = None
-    font_size: int | None = Field(None, ge=8, le=40, multiple_of=2)
+    font_size: int = Field(None, ge=8, le=40, multiple_of=2)
 
 
 class Metadata(BaseModel):
@@ -49,9 +48,4 @@ class UserRequest(BaseModel):
 
 
 class User(UserRequest):
-    id: int
-
-
-class UserImage(BaseModel):
-    file: File
-    description: str | None = None
+    id: PositiveInt
