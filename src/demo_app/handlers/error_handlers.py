c1f57123708089815ef774096cb30bc3c0f69dce
Yugo Kato <yugokato.o@gmail.com>
2025-12-18 07:18:42 -0800

    Add error handler for 405

--- src/demo_app/handlers/error_handlers.py
+++ src/demo_app/handlers/error_handlers.py
@@ -5,7 +5,7 @@ from typing import Any
 from quart import Blueprint, Response, jsonify, make_response, request
 from quart import current_app as app
 from quart_schema import RequestSchemaValidationError
-from werkzeug.exceptions import BadRequest, NotFound, Unauthorized
+from werkzeug.exceptions import BadRequest, MethodNotAllowed, NotFound, Unauthorized
 
 bp_error_handler = Blueprint("error_handler", __name__)
 
@@ -17,22 +17,28 @@ class Error:
     request_id: str
 
 
-@bp_error_handler.app_errorhandler(400)
+@bp_error_handler.app_errorhandler(BadRequest)
 async def handle_bad_request_error(error: BadRequest) -> Response:
-    err = Error(code=400, message=str(error), request_id=request.headers["X-Request-ID"])
-    return await make_response(jsonify({"error": err}), 400)
+    err = Error(code=BadRequest.code, message=str(error), request_id=request.headers["X-Request-ID"])
+    return await make_response(jsonify({"error": err}), BadRequest.code)
 
 
-@bp_error_handler.app_errorhandler(401)
+@bp_error_handler.app_errorhandler(Unauthorized)
 async def handle_unauthorized_request(error: Unauthorized) -> Response:
-    err = Error(code=401, message="Login required", request_id=request.headers["X-Request-ID"])
-    return await make_response(jsonify({"error": err}), 401)
+    err = Error(code=Unauthorized.code, message="Login required", request_id=request.headers["X-Request-ID"])
+    return await make_response(jsonify({"error": err}), Unauthorized.code)
 
 
-@bp_error_handler.app_errorhandler(404)
+@bp_error_handler.app_errorhandler(NotFound)
 async def handle_not_found_error(error: NotFound) -> Response:
-    err = Error(code=404, message=error.description, request_id=request.headers["X-Request-ID"])
-    return await make_response(jsonify({"error": err}), 404)
+    err = Error(code=NotFound.code, message=error.description, request_id=request.headers["X-Request-ID"])
+    return await make_response(jsonify({"error": err}), NotFound.code)
+
+
+@bp_error_handler.app_errorhandler(MethodNotAllowed)
+async def handle_method_not_allowed_error(error: MethodNotAllowed) -> Response:
+    err = Error(code=MethodNotAllowed.code, message=error.description, request_id=request.headers["X-Request-ID"])
+    return await make_response(jsonify({"error": err}), MethodNotAllowed.code)
 
 
 @bp_error_handler.app_errorhandler(RequestSchemaValidationError)
